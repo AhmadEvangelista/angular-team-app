77cdb325bfa82dd102e1fdd7f23d3e723faf5d60
Ahmad P. Evangelista <Ahmad.Evangelista@cebupacificair.com>
2022-08-29 15:46:02 +0800

    functionality of add name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-team-app';
+  newMember: string = '';
+  members: string[] = [];
+
+  onClickAdd() {
+    this.members.push(this.newMember);
+    this.newMember = '';
+  }
+
+  onClickGenerate() {
+    console.log('GENERATE CLICKED');
+  }
+
+  onChangeName(member: string) {
+    this.newMember = member;
+  }
 }
