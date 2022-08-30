f5bdf98630f9cc246ebc4263e13d0d7c42f63cf9
Ahmad P. Evangelista <Ahmad.Evangelista@cebupacificair.com>
2022-08-30 13:21:00 +0800

    generate team functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,13 @@ export class AppComponent {
   newMember: string = '';
   members: string[] = [];
   errorMessage: string = '';
+  numberOfTeams: number | '' = '';
+  teams: string[][] = [];
+
+  onChangeName(member: string) {
+    this.newMember = member;
+    this.errorMessage = '';
+  }
 
   onClickAdd() {
     if (!this.newMember) {
@@ -20,12 +27,38 @@ export class AppComponent {
     this.newMember = '';
   }
 
-  onClickGenerate() {
-    console.log('GENERATE CLICKED');
+  onChangeNumberOfTeams(number: string) {
+    this.numberOfTeams = Number(number);
+    this.errorMessage = '';
   }
 
-  onChangeName(member: string) {
-    this.newMember = member;
-    this.errorMessage = '';
+  onClickGenerate() {
+    if (
+      !this.numberOfTeams ||
+      this.numberOfTeams <= 0 ||
+      this.numberOfTeams > this.members.length
+    ) {
+      this.errorMessage = 'Invalid number of teams';
+      return;
+    }
+
+    const allMembers = [...this.members];
+
+    while (allMembers.length) {
+      for (let i = 0; i < this.numberOfTeams; i++) {
+        const randomIndex = Math.floor(Math.random() * allMembers.length);
+        const member = allMembers.splice(randomIndex, 1)[0];
+
+        if (!member) break;
+        if (this.teams[i]) {
+          this.teams[i].push(member);
+        } else {
+          this.teams[i] = [member];
+        }
+      }
+    }
+
+    this.members = [];
+    this.numberOfTeams = '';
   }
 }
